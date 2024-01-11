17c269f926e70161aa71efe5cc3bef76f39e85af
Muking3 <marcmusasa04@gmail.com>
2024-01-11 10:03:53 +0200

    update

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,9 +1,9 @@
 export default function Navbar({ children, label }) {
     return (
         <>
-            <div className="flex gap-[20px] ml-[13px] mb-[21px] mt-[13px]">
+            <div className="flex gap-[20px] ml-[13px] mb-[21px] mt-[13px] h-[50px]">
                 <div className="w-[25px]">{children}</div>
-                <span>{label}</span>
+                <span className="text-[19px] font-bold">{label}</span>
             </div>
         </>
     )
